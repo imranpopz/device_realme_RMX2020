5f5b48ef1148ec28a19fe04748f336748668ab41
Erfan Abdi <erfangplus@gmail.com>
2021-07-25 07:25:39 +0000

    RMX2020 : Create gatekeeper symlink
    * also fixed typo of chip version in vulkan symlink

--- Android.mk
+++ Android.mk
@@ -19,10 +19,24 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),RMX2020)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+$(GATEKEEPER_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libSoftGatekeeper.so $@
+
+GATEKEEPER_SYMLINKK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.mt6768.so
+GATEKEEPER_SYMLINKK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.oppo6769.so
+GATEKEEPER_SYMLINKK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.mt6768.so
+GATEKEEPER_SYMLINKK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.oppo6769.so
+$(GATEKEEPER_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libMcGatekeeper.so $@
+
 ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_SYMLINK)
 
-VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/vulkan.mt6763.so
-VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.mt6763.so
+VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/vulkan.mt6768.so
+VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.mt6768.so
 $(VULKAN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	@mkdir -p $(dir $@)
 	$(hide) ln -sf ../egl/libGLES_mali.so $@
